c33fd3c887eef9ea1955808e38acf7ddc80576c5
Mike John <eviotamikejohnb@gmail.com>
2023-07-22 03:24:23 +0800

    g
    g

--- src/lib/Auth/User/NotRestrictedUser/MoreDetailVoting/VotingHistory.svelte
+++ src/lib/Auth/User/NotRestrictedUser/MoreDetailVoting/VotingHistory.svelte
@@ -5,7 +5,7 @@
     
 </script>
 
-<section class="border-2 border-black max-w-fit my-2 p-2">
+<section class=" max-w-fit my-2 p-2">
     <div class="border-b-2 border-t-2 border-lightBlue py-1">
         <p class="px-2 bg-lightBlue text-center text-white font-semibold">Voting History</p>
     </div>
